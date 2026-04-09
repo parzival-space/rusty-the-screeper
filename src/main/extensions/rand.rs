ffd6108d84b9a6fccedb4b4cb8f0d38edfc0abc9
Malte Linke <me@parzival.space>
2026-04-09 19:26:45 +0200

    refactor RNG initialization to use current time as seed

--- src/main/extensions/rand.rs
+++ src/main/extensions/rand.rs
@@ -2,7 +2,6 @@ use js_sys::Date;
 use log::trace;
 use rand::{SeedableRng, TryRng};
 use rand_xorshift::XorShiftRng;
-use screeps::game;
 use std::cell::RefCell;
 use std::convert::Infallible;
 use std::option::Option;
@@ -45,10 +44,15 @@ impl TryRng for ScreepsXorShiftRng {
 
 impl ScreepsXorShiftRng {
     fn new() -> Self {
-        let game_time = game::time() as u64;
-        let cpu = (game::cpu::get_used() * 1000.0) as u64;
-        let seed = (game_time ^ (cpu << 32));
-        Self::seed_from_u64(seed)
+        let mut seed = <ScreepsXorShiftRng as SeedableRng>::Seed::default();
+
+        // use current time as seed
+        let current_date = Date::now();
+        for chunk in seed.as_mut().chunks_exact_mut(8) {
+            chunk.copy_from_slice(&current_date.to_le_bytes());
+        }
+
+        Self::from_seed(seed)
     }
 
     pub fn with<F, R>(f: F) -> R
